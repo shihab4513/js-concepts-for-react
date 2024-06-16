071ebb47003e5d56561902963e880c6d3a22c9ec
shihab4513 <shihab4513@gmail.com>
2024-06-16 19:28:13 +0600

    quize.js added .

---
+++ quize.js
@@ -0,0 +1,5 @@
+const result = (5 > 3) ? true : false;
+console.log(result);
+console.log('*********************');
+// Or operator first check whether first one is true if not then it choose next one.Since here "" i.e empty string is falsy it will print "Hello"
+console.log("" || 'Hello');
